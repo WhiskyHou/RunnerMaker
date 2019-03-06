f8f81161f27f773c74b24adea63e964e5ad496b0
whiskyHou_Rog <ghostgaz@126.com>
2019-03-06 11:51:22 +0800

    fps & qualitysettings

---
+++ Assets/Src/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public int __MAX_FPS;
+
+	private FpsLog fpsLog = new FpsLog();
+
+    void Start() {
+		Application.targetFrameRate = __MAX_FPS;    
+    }
+
+    void Update() {
+		float deltaTime = Time.deltaTime;
+
+		fpsLog.Update(deltaTime);
+    }
+}
---
+++ Assets/Src/tools/FpsLog.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FpsLog {
+	private const float __LOG_DELTA_TIME = 5.0f;
+	private float logDeltaTime = 0.0f;
+
+    public void Update(float delta) {
+        if (logDeltaTime > __LOG_DELTA_TIME) {
+			Debug.Log("=== FPS === " + 1.0 / delta);
+			logDeltaTime = 0.0f;
+		} else {
+			logDeltaTime += delta;
+		}
+    }
+}
