a8437ade8d2948309142dbd84024d2562dba29b8
whiskyHou_Rog <ghostgaz@126.com>
2019-03-13 15:10:33 +0800

    场景切换

---
+++ Assets/Src/HomeManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeManager : MonoBehaviour {
+    void Start() {
+		LoadGameScene();
+    }
+
+    void Update() {
+        
+    }
+
+	private async Task LoadGameScene() {
+		await Task.Delay(3000);
+		SceneManager.LoadScene("SampleScene");
+		Debug.Log("===== Load to SmpleScene =====");
+	}
+}
