f987fcc4a02787d3b8abea815a586b07364a92f3
whiskyHou_Rog <ghostgaz@126.com>
2019-03-15 14:23:15 +0800

    大炮和炮弹预制体

--- Assets/Src/Models/Cannon.cs
+++ Assets/Src/Models/Cannon.cs
@@ -2,17 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cannon : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Cannon : MonoBehaviour {
+
+	public float fireDelta;
+
+	public GameObject bullet;
+
+	public GameObject firePoint;
+
+	private float fireTime = 0f;
+
+    void Start() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+		float delta = Time.deltaTime;
+		fireTime += delta;
+		if (fireTime >= fireDelta) {
+			fireTime = 0f;
+			OpenFire();
+		}
     }
+
+	private void OpenFire() {
+		GameObject bulletLeft = Instantiate(bullet);
+		bulletLeft.GetComponent<SharkBullet>().isLeft = true;
+		bulletLeft.transform.SetParent(gameObject.transform);
+		bulletLeft.transform.localPosition = firePoint.transform.localPosition;
+
+		GameObject bulletRight = Instantiate(bullet);
+		bulletRight.GetComponent<SharkBullet>().isLeft = false;
+		bulletRight.transform.SetParent(gameObject.transform);
+		bulletRight.transform.localPosition = firePoint.transform.localPosition;
+	}
 }
--- Assets/Src/Models/SharkBullet.cs
+++ Assets/Src/Models/SharkBullet.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SharkBullet : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class SharkBullet : MonoBehaviour {
+
+	public bool isLeft;
+
+	public float moveSpeed;
+
+    void Start() {
+		if (!isLeft) {
+			gameObject.transform.Find("cannon_bullet").GetComponent<SpriteRenderer>().flipX = true;
+		}
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+	void Update() {
+		float delta = Time.deltaTime;
+		transform.Translate((isLeft ? Vector3.left : Vector3.right) * moveSpeed * delta);
     }
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if(collision.gameObject.tag == "Player") {
+			// player die
+		}
+		//Destroy(gameObject);
+	}
 }
