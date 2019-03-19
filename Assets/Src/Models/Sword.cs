d0f8b1f2ef8159f23332dbf7955726a9db558774
whiskyHou_Rog <ghostgaz@126.com>
2019-03-19 11:55:18 +0800

    旋转光柱预制体

---
+++ Assets/Src/Models/Sword.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+
+	public float rotateSpeed;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+		float delta = Time.deltaTime;
+		transform.Rotate(new Vector3(0f, 0f, delta * rotateSpeed));
+    }
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject.tag == "Player")
+			Debug.Log("=== Sword trigger enter ===");
+	}
+}
