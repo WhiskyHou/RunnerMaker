8d91fc6a13a3546336c1508ee9f379a60a9ba405
whiskyHou_Rog <ghostgaz@126.com>
2019-03-19 15:20:11 +0800

    测试运行时创建预制体

---
+++ Assets/Src/tools/MousePosition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosition : MonoBehaviour {
+
+	public GameObject stone;
+
+	public const float logInterval = 1f;
+
+	private float logDelta = 0f;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+		logDelta += Time.deltaTime;
+
+		Vector3 mousePosOnScene = Input.mousePosition;
+		Vector3 mousePosInWorld = Camera.main.ScreenToWorldPoint(mousePosOnScene);
+
+		if (logDelta > logInterval) {
+			Debug.Log(mousePosInWorld);
+			logDelta = 0f;
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+			GameObject go = Instantiate(stone);
+			go.transform.position = new Vector3((int)Math.Round(mousePosInWorld.x), (int)Math.Round(mousePosInWorld.y), 0f);
+		}
+    }
+}
