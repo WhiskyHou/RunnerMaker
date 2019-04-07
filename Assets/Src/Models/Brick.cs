22203b073720540b89fea775f837f75e796ce161
whiskyHou_workStation <ghostgaz@126.com>
2019-04-07 14:17:21 +0800

    砖块预制体

---
+++ Assets/Src/Models/Brick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    void Start() {
+		
+    }
+
+    void Update() {
+
+    }
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		if(collision.gameObject.tag == "Player") {
+			Destroy(gameObject);
+		}
+	}
+}
