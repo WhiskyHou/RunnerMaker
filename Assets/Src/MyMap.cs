251b6a244c18eceaad34eb99e9496fac8a11f12f
whiskyHou_macbookPro <ghostgaz@126.com>
2019-04-10 16:46:55 +0800

    复原因为愚蠢的vs的Git管理导致的新文件没提交

---
+++ Assets/Src/MyMap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyMap : MonoBehaviour {
+
+	public Text mapname;
+
+	private MyCreationManager manager;
+
+	public void OnClick() {
+		if (manager) {
+			manager.OnClickChooseMap(this);
+		}
+	}
+
+	public void Init(string name, MyCreationManager manager) {
+		mapname.text = name;
+		this.manager = manager;
+	}
+}
