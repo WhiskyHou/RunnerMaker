2d9daa9f9dbb9275662408069797838254d152fd
whiskyHou_macbookPro <ghostgaz@126.com>
2019-04-23 15:18:23 +0800

    add file

---
+++ Assets/Src/ResultWindow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultWindow : MonoBehaviour {
+
+	public Text title;
+
+	public Text content;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+	public void Show(bool isWin, string text) {
+		gameObject.SetActive(true);
+		title.text = isWin ? "通 关 成 功" : "通 关 失 败";
+		content.text = text;
+	}
+}
